0551966bb44e3000e0092d19b771e10085dfef7f
mcsmith01 <mcsmith01@alumni.amherst.edu>
2018-10-10 09:55:21 -0400

    Added testing directions

--- CardTester.java
+++ CardTester.java
@@ -9,5 +9,19 @@ public class CardTester {
 	 */
 	public static void main(String[] args) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		/* Write code that tests your work here. My
+		 * suggestion is:
+		 * Create three cards, two of which are the same
+		 * Print out the result of calls to suit(), rank(),
+		 * and pointValue() for each to ensure the values
+		 * are initialize properly
+		 * Print out the result of a call to toString() for
+		 * one of the cards to ensure it is formatted
+		 * correctly
+		 * Use the matches() method to compare the two that
+		 * are the same and ensure the result is true
+		 * Use the matches() method to compare two that are
+		 * different and ensure the result is false
+		 */
 	}
 }
