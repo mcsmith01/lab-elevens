4d45558531c9d579dcca9fd1c035f680f89a22d6
mcsmith01 <mcsmith01@alumni.amherst.edu>
2019-11-05 15:28:23 -0500

    Moved test methods to relevant classes

--- Card.java
+++ Card.java
@@ -89,4 +89,29 @@ public class Card {
 		return "";
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
 	}
+
+	public static void main(String[] args) {
+		Card card1 = new Card("Ace", "Spades", 11);
+		Card card2 = new Card("Ace", "Spades", 11);
+		Card card3 = new Card("Two", "Clubs", 2);
+		if (card1.matches(card2)) {
+			System.out.println("Good match");
+		}
+		if (!card1.matches(card3)) {
+			System.out.println("Good mismatch");
+		}
+		if (card1.rank().equals("Ace")) {
+			System.out.println("Good rank");
+		}
+		if (card1.suit().equals("Spades")) {
+			System.out.println("Good suit");
+		}
+		if (card1.pointValue() == 11) {
+			System.out.println("Good point value");
+		}
+		if (card1.toString().equals("Ace of Spades (point value = 11)")) {
+			System.out.println("Good toString");
+		}
+	}
+	
 }
--- Deck.java
+++ Deck.java
@@ -101,4 +101,55 @@ public class Deck {
 		rtn = rtn + "\n";
 		return rtn;
 	}
+
+	public static void main(String[] args) {
+		String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+		String[] suits = {"C", "D", "H", "S"};
+		int[] points = {2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11};
+		Deck deck = new Deck(ranks, suits, points);
+		if (deck.size() == ranks.length * suits.length) {
+			System.out.println("Good size");
+		}
+		Card last = deck.deal();
+		for (int i = 1; i < ranks.length * suits.length; i++) {
+			Card next = deck.deal();
+			if (next.matches(last)) {
+				System.out.println("Duplicate card");
+			}
+			last = next;
+		}
+		if (deck.isEmpty()) {
+			System.out.println("Good isEmpty");
+		}
+		/* Uncomment the below lines once you have completed
+		 * activity 4
+		 */
+		 // Deck alt = new Deck(ranks, suits, points);
+		 // activity4Tester(deck, alt);
+	}
+
+	public static void activity4Tester(Deck deck, Deck alt) {
+		deck.shuffle();
+		if (deck.size() == alt.size()) {
+			System.out.println("Good size after shuffle");
+		}
+		Card[] allCards = new Card[alt.size()];
+		for (int i = 0; i < allCards.length; i++) {
+			allCards[i] = alt.deal();
+		}
+		while (!deck.isEmpty()) {
+			Card c = deck.deal();
+			boolean found = false;
+			for (int i = 0; i < allCards.length; i++) {
+				if (allCards[i] != null && c.matches(allCards[i])) {
+					allCards[i] = null;
+					found = true;
+				}
+			}
+			if (!found) {
+				System.out.println("Could not find " + c);
+			}
+		}
+	}
+	
 }
